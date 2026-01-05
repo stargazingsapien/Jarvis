c0836d356279b4eaf9b18a819d8fe2febde7b2e6
stargazingsapien <110345312+stargazingsapien@users.noreply.github.com>
2026-01-06 00:54:28 +0530

    Deleted file

--- start_jarvis.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starting Jarvis...
-cd /d "C:\Users\PARTH\Desktop\projects\AI\AI_Projects\Jarvis"
-"C:\Users\PARTH\Desktop\projects\AI\AI_Projects\.venv\Scripts\python.exe" main.py
-pause
